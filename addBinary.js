72c7d77f10d198b8da0bae03d385e465871aeb10
alihusnain4190 <alihusnain4190@gmail.com>
2020-09-19 00:36:48 +0100

    addBinary

--- addBinary.js
+++ addBinary.js
@@ -1,12 +1,17 @@
-const addBinary = (a, b) => {
-  let sum = a + b;
-  let str = "";
-  while (sum > 0) {
-    const reminder = sum % 2;
-    str = reminder+str;
-    sum = Math.floor(sum / 2);
-  }
+// const addBinary = (a, b) => {
+//   let sum = a + b;
+//   let str = "";
+//   while (sum > 0) {
+//     const reminder = sum % 2;
+//     str = reminder+str;
+//     sum = Math.floor(sum / 2);
+//   }
 
-  console.log(str);
-};
-addBinary(51 , 12 );
+//   console.log(str);
+// };
+
+function addBinary(a,b){
+  return (a+b).toString(2)
+}
+
+console.log(addBinary(51 , 12 ));
