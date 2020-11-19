98ac03ed3d84d7fa295d7d30306150fdca5faf35
alihusnain4190 <alihusnain4190@gmail.com>
2020-11-19 10:18:29 +0000

    solve some katas

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 /* The function sumArgs should accept any number of arguments, and add them together. If given one argument, it should return that. If it is not given arguments, it should return 0 */
 
-function sumArgs(a=0,b=0) {
+function sumArgs(a = 0, b = 0) {
   // let sum = 0;
   // rest.forEach((element) => {
   //  sum = sum + element
   // });
-  return a+b; 
-
+  return a + b;
 }
-
 
 /*
   The function countVeg should take an array of vegetables and a string of the type of vegetable and return the total quantity of that type of vegetable in the array.
@@ -25,12 +23,12 @@ function sumArgs(a=0,b=0) {
 */
 
 function countVeg(input, type) {
- return input.reduce((acc,cur)=>{
-    if(cur.type===type){
-     acc=acc+cur.quantity;
+  return input.reduce((acc, cur) => {
+    if (cur.type === type) {
+      acc = acc + cur.quantity;
     }
     return acc;
- },0)
+  }, 0);
 
   // let counter = 0;
   // for (let i = 0; i < input.length; i++) {
@@ -39,15 +37,99 @@ function countVeg(input, type) {
   //   }
   // }
   // return counter;
+}
+const alternateCase = (input) => {
+  const alter = input.split("").map((c, index) => {
+    if (index % 2 === 0) {
+      return c.toUpperCase();
+    }
+    return c;
+  });
+  return alter.join("");
+};
+const double = (str) => {
+  //  let stack=[];
+  //  str=str.split('');
+  //  for(let i=0;i<str.length;i++){
+  //    if(str[i]===str[i+1]){
+  //      str[i].pop();
+  //    }
+  //    stack.push(str[i]);
+  //  }
+};
 
+/*
+  the areOrdered function should take an array of numbers as an input.
+  It should return true if all the numbers are in ascending order and false if they are not.
+  An empty array should return false.
+*/
+
+function areOrdered(array) {
+  let count = 1;
+  if (array.length === 0) return false;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[i + 1]) {
+      count++;
+    }
+  }
+  if (count === array.length) return true;
+  else {
+    return false;
+  }
 }
-const alternateCase=(input)=>{
-const alter=input.split('').map((c,index)=>{
- if(index%2===0){
-   return c.toUpperCase();
- }
-return c;
+
+/*
+  The orderVeg function should take a array of vegetables and return a new array in which the vegetables are sorted in ascending order according to quantity.
+  e.g.
+  vegCounter([
+  {name: 'Parsnip', type: 'root', quantity: 4},
+  {name: 'Broccoli', type: 'brassica', quantity: 1},
+  {name: 'Carrot', type: 'root', quantity: 5},
+  {name: 'Onion', type: 'bulb', quantity: 3},
+  {name: 'Chard', type: 'leaf', quantity: 3},
+  {name: 'Runner beans', type: 'legume', quantity: 8}
+])
+should return:
+[
+  {name: 'Broccoli', type: 'brassica', quantity: 1},
+  {name: 'Onion', type: 'bulb', quantity: 3},
+  {name: 'Chard', type: 'leaf', quantity: 3},
+  {name: 'Parsnip', type: 'root', quantity: 4},
+  {name: 'Carrot', type: 'root', quantity: 5},
+  {name: 'Runner beans', type: 'legume', quantity: 8}
+]
+*/
+
+function orderVeg(array) {
+  if (array.length === 0) return array;
+
+return array.sort((a,b)=>{
+  
+  return a.quantity-b.quantity;
 })
-return alter.join('')
 }
-module.exports = {sumArgs,countVeg,alternateCase};
+
+/*
+  The function checkUsernames should take an array of usernames and return true if they are all valid and false if any are not valid.
+  A valid username:
+  - is at least 5 characters long
+  - may only contain lowercase letters, numbers and underscores
+  - is no longer than 20 characters
+*/
+
+function checkUsernames(input) {
+  const regix = /[a-z]+\d*_/g;
+  const res = regix.test(input);
+
+  return res;
+}
+
+module.exports = {
+  sumArgs,
+  countVeg,
+  alternateCase,
+  double,
+  areOrdered,
+  orderVeg,
+  checkUsernames
+};
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,11 +1,17 @@
-const {sumArgs,countVeg,alternateCase} = require("../app");
+const {
+  sumArgs,
+  countVeg,
+  alternateCase,
+  double,
+  areOrdered,
+  orderVeg,
+  checkUsernames,
+} = require("../app");
 describe("sumArgs", () => {
   it("add all passing argument", () => {
-    
     expect(sumArgs(1, 2)).toBe(3);
   });
   it("return same argument if you passed one", () => {
-    
     expect(sumArgs(1)).toBe(1);
   });
   it("return 0 if you passed not any argument", () => {
@@ -13,8 +19,6 @@ describe("sumArgs", () => {
     expect(sumArgs(1)).toBe(1);
   });
 });
-
-
 
 describe("countVeg", () => {
   it("function returns a number", () => {
@@ -24,7 +28,7 @@ describe("countVeg", () => {
       { name: "Carrot", type: "root", quantity: 5 },
       { name: "Onion", type: "bulb", quantity: 3 },
       { name: "Chard", type: "leaf", quantity: 3 },
-      { name: "Runner beans", type: "legume", quantity: 8 }
+      { name: "Runner beans", type: "legume", quantity: 8 },
     ];
     const expectedOutput = 9;
     const actualOutput = countVeg(input, "root");
@@ -37,29 +41,27 @@ describe("countVeg", () => {
       { name: "Carrot", type: "root", quantity: 5 },
       { name: "Onion", type: "bulb", quantity: 3 },
       { name: "Chard", type: "leaf", quantity: 3 },
-      { name: "Runner beans", type: "legume", quantity: 8 }
+      { name: "Runner beans", type: "legume", quantity: 8 },
     ];
     const expectedOutput = 0;
     const actualOutput = countVeg(input, "house");
     expect(actualOutput).toBe(expectedOutput);
   });
-})
+});
 
-
-describe('alternateCase', () => {
-  it('describe what happens if this test succeeds here', () => {
+describe("alternateCase", () => {
+  it("describe what happens if this test succeeds here", () => {
     // expect(alternateCase(???)).to...
-    const input='hello';
-    const expectedOutput='HeLlO';
-    const actualOutPut=alternateCase(input);
+    const input = "hello";
+    const expectedOutput = "HeLlO";
+    const actualOutPut = alternateCase(input);
     expect(actualOutPut).toBe(expectedOutput);
- 
   });
-  it('Including space and try to capitalize element', () => {
+  it("Including space and try to capitalize element", () => {
     // expect(alternateCase(???)).to...
-    const input='hello world';
-    const expectedOutput='HeLlO WoRlD';
-    const actualOutPut=alternateCase(input);
+    const input = "hello world";
+    const expectedOutput = "HeLlO WoRlD";
+    const actualOutPut = alternateCase(input);
     expect(actualOutPut).toBe(expectedOutput);
   });
 
@@ -70,4 +72,70 @@ describe('alternateCase', () => {
   //   const actualOutPut=alternateCase(input);
   //   expect(actualOutPut).toBe(expectedOutput);
   // });
+});
+// describe('double',()=>{
+//   it('remove double consective charactor',()=>{
+//     const input='abb';
+//     const output='a';
+//     expect(double(input)).toBe(output);
+//   })
+//   //  it.only('remove double consective charactor',()=>{
+//   //   const input='abbaa';
+//   //   const output='a';
+//   //   expect(double(input)).toBe(output);
+//   // })
+// })
+
+describe("areOrdered", () => {
+  it("return false if you pass empty array", () => {
+    // expect(areOrdered(???)).to...
+    expect(areOrdered([])).toBe(false);
+  });
+  it("return true if pass array in ascending order", () => {
+    // expect(areOrdered(???)).to...
+    expect(areOrdered([1, 2, 3, 4, 5])).toBe(true);
+  });
+  it("return false if passed array not in  ascending order", () => {
+    // expect(areOrdered(???)).to...
+    expect(areOrdered([1, 2, 6, 4, 5])).toBe(false);
+  });
+});
+
+describe("orderVeg", () => {
+  it("return empty arrya if passed empty array", () => {
+    const input = [];
+    const output = [];
+    expect(orderVeg(input)).toEqual(output);
+  });
+  it("return a new array which sort array of object in asscending order", () => {
+    const input = [
+      { name: "Parsnip", type: "root", quantity: 4 },
+      { name: "Broccoli", type: "brassica", quantity: 1 },
+      { name: "Carrot", type: "root", quantity: 5 },
+      { name: "Onion", type: "bulb", quantity: 3 },
+      { name: "Chard", type: "leaf", quantity: 3 },
+      { name: "Runner beans", type: "legume", quantity: 8 },
+    ];
+    const output = [
+      { name: "Broccoli", type: "brassica", quantity: 1 },
+      { name: "Onion", type: "bulb", quantity: 3 },
+      { name: "Chard", type: "leaf", quantity: 3 },
+      { name: "Parsnip", type: "root", quantity: 4 },
+      { name: "Carrot", type: "root", quantity: 5 },
+      { name: "Runner beans", type: "legume", quantity: 8 },
+    ];
+    expect(orderVeg(input)).toEqual(output);
+  });
+});
+describe.only("checkUsernames", () => {
+  it("return true if you pass proper required user name", () => {
+    
+    const input = "myphone_123";
+    expect(checkUsernames(input)).toBe(true);
+  });
+  it("return falase if you pass proper required user name", () => {
+  
+    const input = "1231213";
+    expect(checkUsernames(input)).toBe(false);
+  });
 });
